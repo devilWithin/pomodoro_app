e2f345b0e23a56577a3f6745db3d6c7729df9c61
Mostafa Medhat <mostafamedhat544@gmail.com>
2021-08-22 19:02:10 +0200

    Fourth commit: Change the timer functionality

--- lib/cubit/cubit.dart
+++ lib/cubit/cubit.dart
@@ -11,42 +11,28 @@ import 'package:pomodoro_app/screens/pomodoro_screen.dart';
 import 'package:pomodoro_app/screens/settings_screen.dart';
 
 class PomodoroCubit extends Cubit<PomodoroStates> {
-
   PomodoroCubit() : super(PomodoroInitialState());
 
   static PomodoroCubit get(context) => BlocProvider.of(context);
 
   int currentIndex = 0;
   bool pomodoroOff = true;
-  int seconds = 00;
-  int minutes = 1;
+  late int currentSeconds = totalSeconds;
+  int totalSeconds = 1500;
   Timer? timer;
-  double percent = 0.0;
-
-  ///make it in a separate cubit
-  void percentProgressIndicator() {
-    timer = Timer.periodic(Duration(seconds: 15), (timer) {
-      percent += 15;
-      if(percent >= 100) {
-        timer.cancel();
-        emit(TimerStopped());
-        print("Pomodoro Complete");
-      }
-    });
-  }
 
   void changeIndex(int index) {
     currentIndex = index;
-      emit(ChangeBottomNavBarState());
+    emit(ChangeBottomNavBarState());
   }
 
   Widget screens() => [
-    PomodoroScreen(),
-    PomodoroHistory(),
-    SettingsScreen(),
-  ][currentIndex];
+        PomodoroScreen(),
+        PomodoroHistory(),
+        SettingsScreen(),
+      ][currentIndex];
 
-void addPomodoro(String name) {
+  void addPomodoro(String name) {
     Pomodoro pomodoro = Pomodoro()
       ..date = DateTime.now()
       ..name = name;
@@ -65,17 +51,15 @@ void addPomodoro(String name) {
   }
 
   void stopTimer() {
-    if(state is TimerReset) {
+    if (timer == null) {
       return null;
     } else {
-      emit(TimerReset());
-      percent = 0.0;
-      pomodoroOff = true;
       timer!.cancel();
       timer = null;
-      seconds = 0;
-      minutes = 25;
-      print('Timer reset');
+      pomodoroOff = true;
+      currentSeconds = 1500;
+      emit(TimerStopped());
+      print('Timer stopped');
     }
   }
 
@@ -83,19 +67,27 @@ void addPomodoro(String name) {
     print('Timer Started');
     timer = Timer.periodic(Duration(seconds: 1), (timer) {
       emit(TimerStarted());
-      print('Timer is working');
-      if (seconds > 0) {
-        seconds--;
-      } else {
-        if (minutes > 0) {
-          seconds = 59;
-          minutes--;
-        } else {
-          timer.cancel();
-          emit(TimerStopped());
-          print("Timer Complete");
-        }
+      currentSeconds--;
+      if (currentSeconds <= 0) {
+        timer.cancel();
+        emit(TimerStopped());
       }
+
+      // emit(TimerStarted());
+      // print('Timer is working');
+      // if (seconds > 0) {
+      //   seconds--;
+      // } else {
+      //   if (minutes > 0) {
+      //     seconds = 59;
+      //     minutes--;
+      //   } else {
+      //     timer.cancel();
+      //     pomodoroOff = true;
+      //     emit(TimerStopped());
+      //     print("Timer Complete");
+      //   }
+      // }
     });
   }
 
@@ -105,7 +97,7 @@ void addPomodoro(String name) {
       timer = null;
       emit(PomodoroIsPaused());
       print('Timer paused');
-    } else if(timer == null) {
+    } else if (timer == null) {
       startTimer();
       emit(PomodoroIsPlayed());
       print('Timer started');
--- lib/screens/pomodoro_screen.dart
+++ lib/screens/pomodoro_screen.dart
@@ -18,6 +18,9 @@ class PomodoroScreen extends StatelessWidget {
     return BlocConsumer<PomodoroCubit, PomodoroStates>(
       listener: (context, state) {},
       builder: (context, state) {
+
+        var cubit = PomodoroCubit.get(context);
+
         return SingleChildScrollView(
           child: SafeArea(
             child: Form(
@@ -38,7 +41,7 @@ class PomodoroScreen extends StatelessWidget {
                     ),
                     SizedBox(height: 50),
                     CircularPercentIndicator(
-                      percent: PomodoroCubit.get(context).percent / 100,
+                      percent: (cubit.totalSeconds -  cubit.currentSeconds) / cubit.totalSeconds,
                       radius: 350.0,
                       lineWidth: 20.0,
                       progressColor: kHeavyPinkColor,
@@ -53,14 +56,7 @@ class PomodoroScreen extends StatelessWidget {
                           Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              Text(
-                                "${time.format(PomodoroCubit.get(context).minutes)}:${time.format(PomodoroCubit.get(context).seconds)}",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 60,
-                                  color: kHeavyPurpleColor,
-                                ),
-                              ),
+                              TimerText(sec: cubit.currentSeconds),
                             ],
                           ),
                           SizedBox(height: 20),
@@ -86,7 +82,7 @@ class PomodoroScreen extends StatelessWidget {
                             radius: 40,
                             backgroundColor: kHeavyPinkColor,
                             child:
-                                PomodoroCubit.get(context).pomodoroOff == true
+                                cubit.pomodoroOff == true
                                     ? Icon(
                                         Icons.play_arrow,
                                         color: Colors.white,
@@ -99,7 +95,7 @@ class PomodoroScreen extends StatelessWidget {
                                       ),
                           ),
                           onTap: () {
-                            PomodoroCubit.get(context).pauseOrPlay();
+                            cubit.pauseOrPlay();
                           },
                         ),
                         SizedBox(width: 20),
@@ -114,125 +110,12 @@ class PomodoroScreen extends StatelessWidget {
                             ),
                           ),
                           onTap: () {
-                            PomodoroCubit.get(context).stopTimer();
+                            cubit.stopTimer();
                           },
                         ),
                       ],
                     ),
                     const SizedBox(height: 20),
-                    // TextButton(
-                    //   onPressed: () {
-                    //     showDialog(
-                    //         barrierDismissible: false,
-                    //         context: context,
-                    //         builder: (context) {
-                    //           return Container(
-                    //             height: 400,
-                    //             child: AlertDialog(
-                    //               elevation: 0.5,
-                    //               backgroundColor: Colors.white,
-                    //               title: Text(
-                    //                 'Add Pomodoro',
-                    //                 style: TextStyle(
-                    //                   color: kHeavyPinkColor,
-                    //                   fontWeight: FontWeight.bold,
-                    //                 ),
-                    //               ),
-                    //               content: Container(
-                    //                 height: 200,
-                    //                 child: Column(
-                    //                   children: [
-                    //                     TextFormField(
-                    //                       decoration: InputDecoration(
-                    //                         focusedBorder: OutlineInputBorder(
-                    //                           borderSide: BorderSide(
-                    //                             color: kHeavyPinkColor,
-                    //                           ),
-                    //                         ),
-                    //                         enabledBorder: OutlineInputBorder(
-                    //                           borderSide: BorderSide(
-                    //                             color: kHeavyPinkColor,
-                    //                           ),
-                    //                         ),
-                    //                         labelText: 'Name',
-                    //                         labelStyle: TextStyle(
-                    //                           color: kHeavyPinkColor,
-                    //                           fontWeight: FontWeight.bold,
-                    //                         ),
-                    //                       ),
-                    //                       controller: nameController,
-                    //                       keyboardType: TextInputType.name,
-                    //                       validator: (value) {
-                    //                         if (value!.isEmpty) {
-                    //                           return 'Please Enter the name of the Pomodoro';
-                    //                         }
-                    //                         return null;
-                    //                       },
-                    //                     ),
-                    //                     SizedBox(height: 20),
-                    //                   ],
-                    //                 ),
-                    //               ),
-                    //               actions: [
-                    //                 Row(
-                    //                   mainAxisAlignment:
-                    //                       MainAxisAlignment.spaceBetween,
-                    //                   children: [
-                    //                     TextButton(
-                    //                       onPressed: () {
-                    //                         Navigator.pop(context);
-                    //                         nameController.clear();
-                    //                       },
-                    //                       style: TextButton.styleFrom(
-                    //                         backgroundColor: Colors.white,
-                    //                       ),
-                    //                       child: Text(
-                    //                         'Cancel',
-                    //                         style: TextStyle(
-                    //                           color: kHeavyPinkColor,
-                    //                         ),
-                    //                       ),
-                    //                     ),
-                    //                     TextButton(
-                    //                       onPressed: () {
-                    //                         PomodoroCubit.get(context)
-                    //                             .startTimer();
-                    //                         PomodoroCubit.get(context)
-                    //                             .addPomodoro(
-                    //                           nameController.text,
-                    //                         );
-                    //                         Navigator.pop(context);
-                    //                       },
-                    //                       style: TextButton.styleFrom(
-                    //                         backgroundColor: kHeavyPinkColor,
-                    //                       ),
-                    //                       child: Text(
-                    //                         'Add',
-                    //                         style: TextStyle(
-                    //                           color: Colors.white,
-                    //                         ),
-                    //                       ),
-                    //                     ),
-                    //                   ],
-                    //                 ),
-                    //               ],
-                    //             ),
-                    //           );
-                    //         });
-                    //   },
-                    //   child: Text(
-                    //     'Start a new Pomodoro',
-                    //     style: TextStyle(
-                    //       color: Colors.white,
-                    //       fontSize: 22,
-                    //       fontWeight: FontWeight.bold,
-                    //     ),
-                    //   ),
-                    //   style: TextButton.styleFrom(
-                    //     backgroundColor: kHeavyPinkColor,
-                    //     elevation: 0.0,
-                    //   ),
-                    // ),
                   ],
                 ),
               ),
@@ -240,6 +123,29 @@ class PomodoroScreen extends StatelessWidget {
           ),
         );
       },
+    );
+  }
+}
+
+class TimerText extends StatelessWidget {
+
+  final int sec;
+
+  const TimerText({Key? key,required this.sec}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+   final minutes = sec ~/ 60;
+   final min = minutes < 10 ? '0' + minutes.toString() : minutes.toString();
+   final seconds = sec % 60 < 10 ? '0' + (sec % 60).toString() : sec % 60;
+    return Text(
+      "$min:$seconds",
+      style: TextStyle(
+        fontWeight: FontWeight.w700,
+        fontSize: 60,
+        color: kHeavyPurpleColor,
+      ),
     );
   }
 }
